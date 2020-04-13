2fc76e3d23e5134a96a1ee4bc18a620f398dcef9
Meet Shah <montu48@gmail.comm>
2020-04-13 23:37:55 +0530

    Added Popular and Now Playing Movies

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('layouts.show');
-});
+Route::get('/','MoviesController@index')->name('movies.index');
+Route::get('/movies/{movie}','MoviesController@show')->name('movies.show');
