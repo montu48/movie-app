cbf9d15d78b99b32112a4eda6af73fcb63b63d1c
Meet Shah <montu48@gmail.comm>
2020-04-15 16:51:17 +0530

    Added Search Spinner & TestCase

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,8 +1,11 @@
 <div class="relative mt-3 md:mt-0">
     <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
-    <div class="abosolute top-0">
-
+    <div class="absolute top-0">
+        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
+
+    <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
+
     @if (strlen($search) > 2)
 
     <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
@@ -14,9 +17,10 @@
             <a href="{{route('movies.show',$movie['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
             <img src="https://image.tmdb.org/t/p/w92/{{ $movie['poster_path'] }}" alt="poster" class="w-8">
             @else
-                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+            <a href="{{route('movies.show',$movie['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                <img src="https://via.placeholder.com/32x48" alt="poster" class="w-8">
             @endif
-            <span>{{$movie['title']}}</span>
+            <span class="ml-4">{{$movie['title']}}</span>
         </a>
             </li>
             @endforeach
