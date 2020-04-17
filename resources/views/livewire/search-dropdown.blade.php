ee38dcc999289a893b8a52b86b9ff68f65f5c1b0
Meet Shah <montu48@gmail.com>
2020-04-17 10:56:36 +0530

    UI Interactivity w/ AlpineJs

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,19 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen:true }" @click.away="isOpen = false">
+    <input wire:model.debounce.500ms="search" type="text"
+    class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+    placeholder="Search"
+    x-ref="search"
+    @keydown.window="
+        if(event.keyCode === 191){
+            event.preventDefault();
+            $refs.search.focus();
+        }
+    "
+    @focus="isOpen = true"
+    @keydown.escape.window="isOpen = false"
+    @keydown.shift.tab="isOpen = false"
+    @keydown="isOpen = true"
+    >
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
@@ -8,13 +22,18 @@
 
     @if (strlen($search) > 2)
 
-    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+    <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4"
+        x-show.transition.opacity="isOpen"
+        >
         @if ($searchResults->count() > 0)
         <ul>
             @foreach ($searchResults as $movie)
             <li class="border-b border-gray-700">
+            <a href="{{route('movies.show',$movie['id'])}}"
+            class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150"
+            @if($loop->last) @keydown.tab="isOpen = false" @endif
+            >
             @if($movie['poster_path'])
-            <a href="{{route('movies.show',$movie['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
             <img src="https://image.tmdb.org/t/p/w92/{{ $movie['poster_path'] }}" alt="poster" class="w-8">
             @else
             <a href="{{route('movies.show',$movie['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
